a5a99a3bff773a569d7af003674c4c0fff1141d9
ufst-my <74546460+ufst-my@users.noreply.github.com>
2024-11-28 10:35:09 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
+// After commit: run: https://purge.jsdelivr.net/gh/ufst-my/bookmarklet-checker@main/bookmarklet.js
 javascript:(function() {
     var inputIds = prompt('Indtast fordringsIDs adskilt af komma eller linjeskift:', '');
     if (!inputIds) return;
